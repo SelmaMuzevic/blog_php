590d5e839e9bb86476f8d11a8131c541312da04b
selma <selma@gmail.com>
2017-06-06 17:01:23 +0200

    utilisateur et mod de passe sur login.php

---
+++ login.php
@@ -0,0 +1,22 @@
+<?php
+if(isset($_POST['pseudo'])
+    && isset($_POST['mdp'])){
+        $pseudo = $_POST['pseudo'];
+        $mdp = md5($_POST['mdp']);
+
+        if(is_file("utilisateur/".$pseudo.".txt")){
+            $contenu = file_get_contents("utilisateur/".$pseudo.".txt");
+            if($contenu == $mdp) {
+                echo 'Vous êtes bien conecté.e';
+
+            }else{
+	                echo 'le mot de passe n\'est pas bon';
+	            }
+	            
+	        } else {
+	            echo 'l\'utilisateur '.$pseudo.' n\'existe pas';
+	        }
+	    }
+
+
+?>
